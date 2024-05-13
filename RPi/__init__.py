79a8f45329a8dc92734804f6a45eadfef80beaa3
kadigh <kadigh@bocals-iMac-2.local>
2024-05-13 10:38:25 +0100

    start the code

---
+++ RPi/__init__.py
@@ -0,0 +1,2 @@
+# just to make this directory a package
+# and to make it possible to import the modules in this directory in order to compile this code in a macos environment
---
+++ rccar.py
@@ -0,0 +1,56 @@
+import pygame
+
+import RPi.GPIO as GPio
+
+GPio.setmode(GPio.BCM)
+GPio.setup(18, GPio.OUT)  # Motor 1 Pin 1
+GPio.setup(23, GPio.OUT)  # Motor 1 Pin 2
+GPio.setup(24, GPio.OUT)  # Motor 2 Pin 1
+GPio.setup(25, GPio.OUT)  # Motor 2 Pin 2
+
+# pygame.init()
+# joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
+# dualsense = None
+# for joystick in joysticks:
+#     if joystick.get_name() == "PS5 Controller":
+#         dualsense = joystick
+#         dualsense.init()
+#         break
+
+# def control_motors():
+#     # Get the D-pad button states
+#     dpad_up = dualsense.get_button(0)
+#     dpad_down = dualsense.get_button(1)
+#     dpad_left = dualsense.get_button(2)
+#     dpad_right = dualsense.get_button(3)
+    
+#     # Control Motor 1
+#     if dpad_up:
+#         GPIO.output(18, GPIO.HIGH)
+#         GPIO.output(23, GPIO.LOW)
+#     elif dpad_down:
+#         GPIO.output(18, GPIO.LOW)
+#         GPIO.output(23, GPIO.HIGH)
+#     else:
+#         GPIO.output(18, GPIO.LOW)
+#         GPIO.output(23, GPIO.LOW)
+    
+#     # Control Motor 2
+#     if dpad_right:
+#         GPIO.output(24, GPIO.HIGH)
+#         GPIO.output(25, GPIO.LOW)
+#     elif dpad_left:
+#         GPIO.output(24, GPIO.LOW)
+#         GPIO.output(25, GPIO.HIGH)
+#     else:
+#         GPIO.output(24, GPIO.LOW)
+#         GPIO.output(25, GPIO.LOW)
+
+# while True:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             pygame.quit()
+#             GPIO.cleanup()
+#             quit()
+    
+#     control_motors()
